91d0492bfee1dc14a7ebda9228ddfa406b700a28
Edgar <mmnledgar@gmail.com>
2020-10-16 13:03:58 +0000

    adding_project_views

---
+++ WebContent/WEB-INF/views/home/home.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css"  href=<c:out value="${base}public/css/home.css"/> />
+<title>HOME</title>
+</head>
+<body>
+	<div class="body_tert">
+		<div class="right">
+			<h2>SEN FORAGE</h2>
+			<h1>RED</h1>
+		</div>
+	</div>
+</body>
+</html>
---
+++ src/sn/forage/web/HomeController.java
@@ -0,0 +1,36 @@
+package sn.forage.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="home",urlPatterns = {"/home"})
+public class HomeController extends HttpServlet {
+	@Override
+	public void init() throws ServletException {
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("base", "http://localhost:8081/JEE_JSP_SERVLET_JPA/");
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/home/home.jsp");
+		rd.forward(req, resp);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+	
+	
+	
+	
+	
+}
